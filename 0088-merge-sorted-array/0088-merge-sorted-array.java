91f4103d0619bdbc2778b05a2621d843158d5ac7
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-20 21:37:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (60.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,28 @@
 class Solution {
+    /*
+        Technique: Two Pointers
+        create two pointers pointing to end of both the arrays. Now compare both the element and add the largest to end of nums1 array. Repeat the process until one of the pointer goes out of bounds. Next include any unprocessed elements from any of the arrays.
+    
+        TC: O(N + M) SC: O(1)
+    */
+
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] temp = new int[n + m];
-        int p1 = 0, p2 = 0;
-        int ptr = 0;
-        while (p1 < m && p2 < n) {
-            if (nums1[p1] <= nums2[p2])
-                temp[ptr++] = nums1[p1++];
+        int p1 = m - 1, p2 = n - 1;
+        int ptr = n + m - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2])
+                nums1[ptr--] = nums1[p1--];
             else
-                temp[ptr++] = nums2[p2++];
+                nums1[ptr--] = nums2[p2--];
         }
 
         // Add unprocessed elements from any of the arrays
-        if (p1 < m) {
-            while (p1 < m)
-                temp[ptr++] = nums1[p1++];
+        if (p1 >= 0) {
+            while (p1 >= 0)
+                nums1[ptr--] = nums1[p1--];
         } else {
-            while (p2 < n)
-                temp[ptr++] = nums2[p2++];
+            while (p2 >= 0)
+                nums1[ptr--] = nums2[p2--];
         }
-
-        // Copy back temp array to nums1
-        int len = n + m;
-        for (int i = 0; i < len; i++)
-            nums1[i] = temp[i];
-
     }
 }

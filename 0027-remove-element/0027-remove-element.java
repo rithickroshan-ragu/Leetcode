19edec493621e1fd7334516b52cbd869727a9aec
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-21 22:07:49 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.55%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,42 +2,24 @@ class Solution {
     /*
         Technique: Two Pointers
     
-        Initialize two pointer at first and last element. Check is nums[s] == val. Then replace
-        with end last pointer. ensure last pointer is not pointing to element == val. Keep count
-        of non k values and return. 
+        Initialize two pointer at start one called index => referring to where non-target element
+        must be placed and another pointer for looping through the array. While traversing place 
+        every non-target element at the index pointer and move the pointers.
     
         TC: O(N) SC: O(1)
     */
 
     public int removeElement(int[] nums, int val) {
         int N = nums.length;
-        int s = 0, e = N - 1;
-        int k = 0;
-        while (s <= e) {
-            if (nums[s] == val) {
-                while (e > s && nums[e] == val) {
-                    e--;
-                }
-
-                if (e == s)
-                    break;
-
-                int temp = nums[e];
-                nums[e] = nums[s];
-                nums[s] = temp;
-                k++;
-                s++;
-                e--;
-            } else {
-                k++;
-                s++;
+        int index = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[index++] = nums[i];
             }
         }
 
-        return k;
-    }
-
-    void swap(int[] nums, int s, int e) {
-
+        return index;
     }
 }

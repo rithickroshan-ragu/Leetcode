8dba52c38159e042f4e2602593606434f6e4f9aa
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-23 16:32:08 +0530

    Time: 22 ms (9.25%), Space: 49.3 MB (93.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,31 @@
 class Solution {
+    /*
+        Technique: Sorting, HashMap
+    
+        Approach 1: Sorting, since element appears n/2 times, the mid element will always be the 
+        majority element => TC: O(N logN) SC: O(1)
+    
+        Approach 2: Count using Hashmap. TC: O(N) SC: O(N)
+    */
+
     public int majorityElement(int[] nums) {
         int N = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
 
         int max_freq = 0, element = Integer.MIN_VALUE;
-        for (int i = 0; i < N ; i++)
-        {
+        for (int i = 0; i < N; i++) {
             int key = nums[i];
             if (map.containsKey(key))
                 map.put(key, map.get(key) + 1);
             else
                 map.put(key, 1);
 
-            if (map.get(key) > max_freq)
-            {
+            if (map.get(key) > max_freq) {
                 max_freq = map.get(key);
                 element = key;
             }
         }
-        
+
         return element;
     }
 }

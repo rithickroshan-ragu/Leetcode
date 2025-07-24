674bae0d6ae26907f5fae14d00a99e9d6d0fb820
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-24 20:13:44 +0530

    Time: 2 ms (72.57%), Space: 61.7 MB (64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,24 +1,45 @@
 class Solution {
     /*
+        Approach1:
         Technique: Dynamic Programming, Recursion
 
         1. Every day we have two option either to BUY/SELL stock or not do anythin. So try all
         possible combinations using recursion.
-
         2. Use DP to optimize the fucntion calls.
+
+        TC: O(N * 2) SC: O(N * 2)
+
+        ===================================================================
+
+        Approach 2: Simpler , array traversal
+
+        1. Simply travrese through the array and keep track of 
+        lowestElementSoFar and profit = (currPrice - lowestElementSoFar). 
+
+        TC: O(N) SC: O(1)
     */
 
     int BUY = 0;
     int SELL = 1;
 
     public int maxProfit(int[] prices) {
-        int N = prices.length;
-        int[][] dp = new int[N][2];
-        for (int[] arr: dp)
+        // int N = prices.length;
+        // int[][] dp = new int[N][2];
+        // for (int[] arr: dp)
+        // {
+        //     Arrays.fill(arr, -1);
+        // }
+        // return transact(prices, 0, BUY, dp);
+
+        int lowestPriceSoFar = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int price: prices)
         {
-            Arrays.fill(arr, -1);
+            lowestPriceSoFar = Math.min(lowestPriceSoFar, price);
+            profit = Math.max(profit, price - lowestPriceSoFar);
         }
-        return transact(prices, 0, BUY, dp);
+
+        return profit;
     }
 
     int transact(int[] prices, int index, int signal, int[][] dp)

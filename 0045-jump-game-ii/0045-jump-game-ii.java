62af21346aba94a92cc7e406a7e62013872cc023
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-29 10:12:50 +0530

    Time: 122 ms (7.85%), Space: 45.8 MB (10.61%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    /*
+        Technique: Dynamic Programming
+
+        Start with first of array and try all possible jumps. Do the same for all elemnents of
+        the array. Store min #jumps while trying all possible paths. Use memoization to optimize.
+
+        TC: O(N) SC: O(N)
+    */
+
+
+    public int jump(int[] nums) {
+        int N = nums.length;
+        int[] dp = new int[N];
+        Arrays.fill(dp, -1);
+        return minJumps(nums, 0, dp);
+    }
+
+    int minJumps(int[] nums, int index, int[] dp) {
+        if (index >= nums.length - 1)
+            return 0;
+        if (dp[index] != -1)
+            return dp[index];
+
+        // Jump forward - all possible jumps
+        int jumps = nums.length;
+        for (int i = 1; i <= nums[index]; i++) {
+            jumps = Math.min(jumps, 1 + minJumps(nums, index + i, dp));
+        }
+
+        return dp[index] = jumps;
+    }
+}

aaf278dd57a0b31328d1d3b0b42530efb9d65cd4
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-30 21:48:56 +0530

    Time: 4 ms (99%), Space: 45.1 MB (93.4%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,27 +1,53 @@
 class Solution {
     /*
+    
+        Approach 1:
         Technique: HashMap
     
         Use hashmap to count frequency of all chars in the string. Now find the first char with
         one occurence.
     
         TC: O(N) SC: O(N)
+    
+        Approach 2:
+        Technique: Freqency array
+    
+        Build a freqency array of 26 chars in alphabet for all chars in the string. now fidn the
+        first char with only one occurence.
+    
+        TC: O(N) SC: O(N)
     */
 
     public int firstUniqChar(String s) {
-        int N = s.length();
-        HashMap<Character, Integer> map = new HashMap<>();
+        // int N = s.length();
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        for (char c : s.toCharArray()) {
-            if (map.containsKey(c))
-                map.put(c, map.get(c) + 1);
-            else
-                map.put(c, 1);
-        }
+        // for (char c : s.toCharArray()) {
+        //     if (map.containsKey(c))
+        //         map.put(c, map.get(c) + 1);
+        //     else
+        //         map.put(c, 1);
+        // }
+
+        // for (int i = 0; i < N; i++) {
+        //     char c = s.charAt(i);
+        //     if (map.get(c) == 1)
+        //         return i;
+        // }
+
+        // return -1;
+
+        /* ======================================================================= */
+
+        int N = s.length();
+        char[] chars = s.toCharArray();
+        int[] freq = new int[26];
+
+        for (int i = 0; i < N; i++)
+            freq[chars[i] - 'a']++;
 
         for (int i = 0; i < N; i++) {
-            char c = s.charAt(i);
-            if (map.get(c) == 1)
+            if (freq[chars[i] - 'a'] == 1)
                 return i;
         }
 

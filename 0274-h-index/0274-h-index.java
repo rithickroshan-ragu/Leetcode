e15c9acb904d7b82dfa40ef1ef68c734f17699f1
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-31 22:10:37 +0530

    Time: 1 ms (73.72%), Space: 41.6 MB (53.19%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -2,11 +2,20 @@ class Solution {
     /*
         Technique: Counting
     
+        Approach 1:
         Count the no.of.papers with i citations. After building the freq array now you can check 
         by traversing the freq array to find hIndex. for every pos see if at lease count[pos]
         papers are there which has >= citations.
     
         TC: O(N) SC: O(N)
+    
+        Approach 2: 
+        Technique: Binary Search
+        Do binary search on possible citations. since the max h_index cannot exceed 
+        len of the citations array. So do a binary search and see if there are n papers with 
+        more than mid citations and repeat the process.
+    
+        TC: O(N logN) SC: O(1)
     */
 
     public int hIndex(int[] citations) {

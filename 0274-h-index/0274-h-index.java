7859a79a27fb75cc6452c0b722ca8119101e794b
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-07-31 22:07:19 +0530

    Time: 1 ms (73.72%), Space: 41.5 MB (67.11%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,17 +1,25 @@
 class Solution {
+    /*
+        Technique: Counting
+    
+        Count the no.of.papers with i citations. After building the freq array now you can check 
+        by traversing the freq array to find hIndex. for every pos see if at lease count[pos]
+        papers are there which has >= citations.
+    
+        TC: O(N) SC: O(N)
+    */
+
     public int hIndex(int[] citations) {
         int N = citations.length;
 
         int[] count = new int[1000 + 1];
-        for (int i: citations)
+        for (int i : citations)
             count[i]++;
 
         int rem = N, len = count.length;
         int hIndex = 0;
-        for (int i = 0; i < len; i++)
-        {
-            if (rem >= i)
-            {
+        for (int i = 0; i < len; i++) {
+            if (rem >= i) {
                 hIndex = i;
                 rem -= count[i];
             }

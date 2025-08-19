de8b5ff024936611be5fb503a34b389026d8a696
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-08-19 19:38:11 +0530

    Time: 13 ms (31.15%), Space: 45.2 MB (37.24%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,37 @@
+class Solution {
+    /*
+        Technique: HashMap
+    
+        1. store count of all characters in magazine in a hashmap.
+        2. for every character in ransomNote check if the char exists in hashmap and redice the count.
+        3. if all chars with req count is present return true else return false.
+    
+        TC: O(N)    SC: O(N)
+    */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> chars = new HashMap<>();
+        char[] arr = magazine.toCharArray();
+        for (char c : arr) {
+            if (chars.containsKey(c))
+                chars.put(c, chars.get(c) + 1);
+            else
+                chars.put(c, 1);
+        }
+
+        arr = ransomNote.toCharArray();
+        for (char c : arr) {
+            if (!chars.containsKey(c)) {
+                return false;
+            } else {
+                int remChars = chars.get(c);
+                remChars--;
+                if (remChars == 0)
+                    chars.remove(c);
+                else
+                    chars.put(c, remChars);
+            }
+        }
+
+        return true;
+    }
+}

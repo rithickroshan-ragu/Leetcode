51ecd33d89a6086681eac530815165d650e7e69d
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-08-23 18:37:17 +0530

    Time: 15 ms (5.59%), Space: 44.1 MB (67.97%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,30 @@
+class Solution {
+    /*
+        Technique: Binary Search
+
+        1. Do a binary search to find if a num is a power of 3.
+        2. n == 3^^x where x can be from 1 ... log3(n).
+        3. Do binary search on this range of powers to see if it matches any num.
+
+        TC: O(log(log3 N))    SC: O(1)
+    */
+
+    public boolean isPowerOfThree(int n) {
+        if (n < 1)
+            return false;
+
+        long s = 0, e = n;
+        while (s <= e) {
+            long mid = (s + e) / 2;
+            long val = (long) Math.pow(3, mid);
+            if (n - val == 0)
+                return true;
+            else if (val < n)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+
+        return false;
+    }
+}

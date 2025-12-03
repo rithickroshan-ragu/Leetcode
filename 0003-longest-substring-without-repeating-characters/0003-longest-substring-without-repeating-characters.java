91bae700b548ed13cdb7f819fb445032d0def51a
Rithickroshan Ragu <55408337+rithickroshan-ragu@users.noreply.github.com>
2025-12-03 22:45:12 +0530

    Time: 6 ms (68.42%), Space: 46.3 MB (21.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,13 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        /*
+            Technique: Two Pointers, HashSet
+        
+            step 1: Use two pointer left and right intilized to 0th index.
+            step 2: Move right pointer and add elements to set. When an existing element is seen
+                    remove elements from start of string (use left pointer) in the set, until you remove 
+                    the duplicate element.
+            step 3: Keep track of the size of set(unique substring elements) while doing this.
+        
+            TC: O(N)    SC: O(N)
+        */
+
         HashSet<Character> set = new HashSet<>();
         int l = 0, r = 0, N = s.length();
         int max = 0;
-        while (r < N)
-        {
+        while (r < N) {
             char curr = s.charAt(r);
-            while(set.contains(curr))
-            {
+            while (set.contains(curr)) {
                 set.remove(s.charAt(l));
                 l++;
             }
